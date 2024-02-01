646a2a717590b97c860bd15b621f07b7fc01f468
VickArmand <victormaina1962@gmail.com>
2024-02-01 18:13:35 +0300

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,7 +16,7 @@ Routes:
 Import this 7-dump to have some data
 You must use the option strict_slashes=False in your route definition
 """
-from flask import Flask
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
@@ -31,11 +31,11 @@ def fetch_states():
         in DBStorage sorted by name (A->Z) tip
         LI tag: description of one State: <state.id>: <B><state.name></B>
     """
-    return render_template("7-states_list.html", data=storage.all(State))
+    return render_template("7-states_list.html", states=storage.all(State))
 
 
 @app.teardown_appcontext
-def closesession():
+def closesession(self):
     """remove the current SQLAlchemy Session"""
     storage.close()
 
