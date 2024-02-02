91464d08dc5a2a841c6e38ef0a4923cafb000717
VickArmand <victormaina1962@gmail.com>
2024-02-02 19:36:56 +0300

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -27,14 +27,14 @@ script that starts a Flask web application:
             H1 tag: “Not found!”
 You must use the option strict_slashes=False in your route definition
 """
-from flask import Flask
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states')
-def states:
+@app.route('/states', strict_slashes=False)
+def states():
     """
     returns a HTML page: (inside the tag BODY)
         H1 tag: “States”
@@ -43,3 +43,33 @@ def states:
         LI tag: description of one State: <state.id>: <B><state.name></B>
     """
     return render_template('9-states.html', states=storage.all(State))
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def single_state(id):
+    """
+    returns a HTML page: (inside the tag BODY)
+        If a State object is found with this id:
+            H1 tag: “State: ”
+            H3 tag: “Cities:”
+            UL tag: with the list of City objects linked to the
+            State sorted by name (A->Z)
+            LI tag: description of one City: <city.id>: <B><city.name></B>
+        Otherwise:
+            H1 tag: “Not found!”
+    """
+    s = {}
+    for state in storage.all(State).values():
+        if id == state.id:
+            s[id] = state
+    return render_template('9-states.html', state=s)
+
+
+@app.teardown_appcontext
+def closesession(self):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
