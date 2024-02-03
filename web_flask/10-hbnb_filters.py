0b28d56decd37a4344d2a53ef4c9d37f1bc3d325
VickArmand <victormaina1962@gmail.com>
2024-02-03 14:06:41 +0300

    resolving filter responsivity

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -31,13 +31,18 @@ script that starts a Flask web application:
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 app = Flask(__name__)
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
     """returns the page 10-hbnb_filters.html """
-    return render_template('10-hbnb_filters.html', states=storage.all(State))
+    return render_template(
+                           '10-hbnb_filters.html',
+                           states=storage.all(State),
+                           amenities=storage.all(Amenity)
+                           )
 
 
 @app.teardown_appcontext
