f5382e1f5a3f2ffdf3f01b2f0d9e596609fff317
tam-dcreator <tam@THE-7TH>
2024-02-24 21:54:33 +0100

    Minor corrections to file

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base, Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
